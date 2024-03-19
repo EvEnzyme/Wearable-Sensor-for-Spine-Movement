ad9d1d1e675571c32248ec2cb6e2d274c18b63c1
abirami-s9 <156034232+abirami-s9@users.noreply.github.com>
2024-03-19 19:39:36 -0400

    Added functions to get information on other axes from both sensors (#4)

--- main.ino
+++ main.ino
@@ -52,12 +52,38 @@ void loop()
 {
   BNO::sAxisAnalog_t   sAccAnalog1 = bno1.getAxis(BNO::eAxisAcc);    // read acceleration
   BNO::sAxisAnalog_t   sAccAnalog2 = bno2.getAxis(BNO::eAxisAcc);    // read acceleration
+  BNO::sAxisAnalog_t   sMagAnalog1 = bno1.getAxis(BNO::eAxisMag); // read geomagnetic
+  BNO::sAxisAnalog_t   sMagAnalog2 = bno2.getAxis(BNO::eAxisMag); // read geomagnetic
+  BNO::sAxisAnalog_t   sGyrAnalog1 = bno1.getAxis(BNO::eAxisGyr); // read gyroscope
+  BNO::sAxisAnalog_t   sGyrAnalog2 = bno2.getAxis(BNO::eAxisGyr); // read gyroscope
+  BNO::sAxisAnalog_t   sLiaAnalog1 = bno1.getAxis(BNO::eAxisLia); // read linear acceleration
+  BNO::sAxisAnalog_t   sLiaAnalog2 = bno2.getAxis(BNO::eAxisLia); // read linear acceleration
+  BNO::sAxisAnalog_t   sGrvAnalog1 = bno1.getAxis(BNO::eAxisGrv); // read gravity vector
+  BNO::sAxisAnalog_t   sGrvAnalog2 = bno2.getAxis(BNO::eAxisGrv); // read gravity vector
+  BNO::sEulAnalog_t   sEulAnalog1 = bno1.getEul();                  // read euler angle
+  BNO::sEulAnalog_t   sEulAnalog2 = bno2.getEul();                  // read euler angle
+  BNO::sQuaAnalog_t   sQuaAnalog1 = bno1.getQua();                  // read quaternion
+  BNO::sQuaAnalog_t   sQuaAnalog2 = bno2.getQua();                  // read quaternion
+
+
   
  
   Serial.println();
   Serial.println("======== analog data print start ========");
   Serial.print("sensor 1 acc analog: (unit mg)       "); printAxisData(sAccAnalog1);
   Serial.print("sensor 2 acc analog: (unit mg)       "); printAxisData(sAccAnalog2);
+  Serial.print("sensor 1 mag analog: (unit ut)       "); printAxisData(sMagAnalog1);
+  Serial.print("sensor 2 mag analog: (unit ut)       "); printAxisData(sMagAnalog2);
+  Serial.print("sensor 1 gyr analog: (unit dps)      "); printAxisData(sGyrAnalog1);
+  Serial.print("sensor 2 gyr analog: (unit dps)      "); printAxisData(sGyrAnalog2);
+  Serial.print("sensor 1 lia analog: (unit mg)       "); printAxisData(sLiaAnalog1);
+  Serial.print("sensor 2 lia analog: (unit mg)       "); printAxisData(sLiaAnalog2);
+  Serial.print("sensor 1 grv analog: (unit mg)       "); printAxisData(sGrvAnalog1);
+  Serial.print("sensor 2 grv analog: (unit mg)       "); printAxisData(sGrvAnalog2);
+  Serial.print("sensor 1 eul analog: (unit degree)   "); Serial.print(" head: "); Serial.print(sEulAnalog1.head); Serial.print(" roll: "); Serial.print(sEulAnalog1.roll);  Serial.print(" pitch: "); Serial.println(sEulAnalog1.pitch);
+  Serial.print("sensor 2 eul analog: (unit degree)   "); Serial.print(" head: "); Serial.print(sEulAnalog2.head); Serial.print(" roll: "); Serial.print(sEulAnalog2.roll);  Serial.print(" pitch: "); Serial.println(sEulAnalog2.pitch);
+  Serial.print("sensor 1 qua analog: (no unit)       "); Serial.print(" w: "); Serial.print(sQuaAnalog1.w); printAxisData(sQuaAnalog1);
+  Serial.print("sensor 2 qua analog: (no unit)       "); Serial.print(" w: "); Serial.print(sQuaAnalog2.w); printAxisData(sQuaAnalog2);
 
   Serial.println("========  analog data print end  ========");
 
