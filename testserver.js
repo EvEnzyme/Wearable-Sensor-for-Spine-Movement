c42323bcf068bb8cfa3089ef7a78eaeb1f7e9c1c
ahkabir48 <ahkabir48@gmail.com>
2024-11-11 17:44:28 -0500

    delete test server; use python socket

--- testserver.js
+++
@@ -1,9 +0,0 @@
-const net = require('net');
-
-let testserver = net.createServer((socket) => {
-	socket.on('data', (data) => {
-		console.log("Received: " + data);
-	});
-	console.log("Accepted connection.");
-	socket.write("Hello from the server!\n");
-}).listen(10200, '127.0.0.1', () => console.log("Listening on 10200."));
